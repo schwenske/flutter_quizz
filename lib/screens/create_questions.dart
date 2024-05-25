8ca98a67c6aa47af74a56192acf047aad32a224e
schwenske <sven-paule@gmx.de>
2024-05-25 21:44:01 +0200

    edit the floating aktion button and the screen

---
+++ lib/screens/create_questions.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CreateQuestions extends StatelessWidget {
+  const CreateQuestions({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("CreateQuestions"),
+      ),
+    );
+  }
+}
