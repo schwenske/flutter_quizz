12a7ed2b85879f527e4726be06324b786076b99c
schwenske <sven.neuefeind@gmx.de>
2024-06-28 11:12:05 +0200

    fragen melden jetzt immer aktiv

--- lib/screens/widgets/question_card_game.dart
+++ lib/screens/widgets/question_card_game.dart
@@ -57,49 +57,16 @@ class _QuestionCardGameState extends State<QuestionCardGame> {
   @override
   Widget build(BuildContext context) {
     bool checkAnswer = true;
-    return AbsorbPointer(
-      absorbing: isBlocked,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(5),
-            child: Card(
-              color: Colors.blue[100],
-              child: ConstrainedBox(
-                constraints: BoxConstraints(
-                  minHeight: MediaQuery.of(context).size.height * 0.1,
-                  minWidth: MediaQuery.of(context).size.width * 0.8,
-                  maxHeight: MediaQuery.of(context).size.height * 0.15,
-                  maxWidth: MediaQuery.of(context).size.width * 0.9,
-                ),
-                child: Text(
-                  widget.questionCard.question,
-                  textAlign: TextAlign.center,
-                  style: DefaultTextStyle.of(context)
-                      .style
-                      .apply(fontSizeFactor: 1.7),
-                ),
-              ),
-            ),
-          ),
-          for (int i = 0; i < widget.questionCard.options.keys.length; i++)
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: Card(
-                color: gameCardColor[i],
-                child: InkWell(
-                  splashColor: Colors.blue,
-                  onTap: () {
-                    setState(() {
-                      if (gameCardColor[i] == Colors.white) {
-                        gameCardColor[i] = Colors.green;
-                        gameCardCurrentBool[i] = true;
-                      } else {
-                        gameCardColor[i] = Colors.white;
-                        gameCardCurrentBool[i] = false;
-                      }
-                    });
-                  },
+    return Column(
+      children: [
+        AbsorbPointer(
+          absorbing: isBlocked,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(5),
+                child: Card(
+                  color: Colors.blue[100],
                   child: ConstrainedBox(
                     constraints: BoxConstraints(
                       minHeight: MediaQuery.of(context).size.height * 0.1,
@@ -108,120 +75,161 @@ class _QuestionCardGameState extends State<QuestionCardGame> {
                       maxWidth: MediaQuery.of(context).size.width * 0.9,
                     ),
                     child: Text(
-                      widget.questionCard.options.keys.elementAt(i),
+                      widget.questionCard.question,
+                      textAlign: TextAlign.center,
                       style: DefaultTextStyle.of(context)
                           .style
-                          .apply(fontSizeFactor: 1.3),
+                          .apply(fontSizeFactor: 1.7),
                     ),
                   ),
                 ),
               ),
-            ),
-          ButtonBar(
-            alignment: MainAxisAlignment.center,
-            children: [
-              CustomButton(
-                  onPressed: () async {
-                    setState(() {
-                      isBlocked = true;
-                      widget.callBoolBack(!isBlocked);
-                    });
-                    for (int i = 0;
-                        i < widget.questionCard.options.keys.length;
-                        i++) {
-                      if ((gameCardColor[i] == Colors.green) &&
-                          (widget.questionCard.options.values.elementAt(i) ==
-                              gameCardCurrentBool[i])) {
-                        correctAnswer[i] = true;
-                      } else if ((gameCardColor[i] == Colors.white) &&
-                          (widget.questionCard.options.values.elementAt(i) ==
-                              gameCardCurrentBool[i])) {
-                        correctAnswer[i] = true;
-                      } else {}
-                    }
-                    for (int i = 0; i < correctAnswer.length; i++) {
-                      if (correctAnswer[i] == false) {
-                        checkAnswer = false;
-                      } else {}
-                    }
-                    if (checkAnswer == false) {
-                      widget.questionCard.options.forEach((key, value) {
-                        if (!value) {
-                          answerList.add(key);
+              for (int i = 0; i < widget.questionCard.options.keys.length; i++)
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Card(
+                    color: gameCardColor[i],
+                    child: InkWell(
+                      splashColor: Colors.blue,
+                      onTap: () {
+                        setState(() {
+                          if (gameCardColor[i] == Colors.white) {
+                            gameCardColor[i] = Colors.green;
+                            gameCardCurrentBool[i] = true;
+                          } else {
+                            gameCardColor[i] = Colors.white;
+                            gameCardCurrentBool[i] = false;
+                          }
+                        });
+                      },
+                      child: ConstrainedBox(
+                        constraints: BoxConstraints(
+                          minHeight: MediaQuery.of(context).size.height * 0.1,
+                          minWidth: MediaQuery.of(context).size.width * 0.8,
+                          maxHeight: MediaQuery.of(context).size.height * 0.15,
+                          maxWidth: MediaQuery.of(context).size.width * 0.9,
+                        ),
+                        child: Text(
+                          widget.questionCard.options.keys.elementAt(i),
+                          style: DefaultTextStyle.of(context)
+                              .style
+                              .apply(fontSizeFactor: 1.3),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
+                children: [
+                  CustomButton(
+                      onPressed: () async {
+                        setState(() {
+                          isBlocked = true;
+                          widget.callBoolBack(!isBlocked);
+                        });
+                        for (int i = 0;
+                            i < widget.questionCard.options.keys.length;
+                            i++) {
+                          if ((gameCardColor[i] == Colors.green) &&
+                              (widget.questionCard.options.values
+                                      .elementAt(i) ==
+                                  gameCardCurrentBool[i])) {
+                            correctAnswer[i] = true;
+                          } else if ((gameCardColor[i] == Colors.white) &&
+                              (widget.questionCard.options.values
+                                      .elementAt(i) ==
+                                  gameCardCurrentBool[i])) {
+                            correctAnswer[i] = true;
+                          } else {}
                         }
-                      });
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title:
-                                  const Text('Folgende Antworten sind falsch:'),
-                              content: SingleChildScrollView(
-                                child: ListBody(
-                                  children: [
-                                    for (int i = 0; i < answerList.length; i++)
-                                      Text(answerList[i]),
-                                    Text(
-                                        '\nBitte beachten Sie dabei folgenden Grund ${widget.questionCard.reason}'),
-                                  ],
-                                ),
-                              ),
-                              actions: [
-                                TextButton(
-                                    child: const Text('ok'),
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    })
-                              ],
-                            );
+                        for (int i = 0; i < correctAnswer.length; i++) {
+                          if (correctAnswer[i] == false) {
+                            checkAnswer = false;
+                          } else {}
+                        }
+                        if (checkAnswer == false) {
+                          widget.questionCard.options.forEach((key, value) {
+                            if (!value) {
+                              answerList.add(key);
+                            }
                           });
-                    } else {
-                      currentCounter = await checkCounter();
-                      if (currentCounter == -1) {
-                        currentUserId = userId;
-                        currentCounter = 5;
-                        countPoints += 5;
-                        _addUserRanking();
-                      } else {
-                        int newCounterValue = currentCounter + 5;
-                        countPoints += 5;
-                        await firestoreQuestionRep.updateCounter(
-                            userId, newCounterValue);
-                      }
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              title: Text(
-                                  '$userName, Sie haben alles richtig beantwortet'),
-                              content: const SingleChildScrollView(
-                                child: ListBody(
-                                  children: [
-                                    Text('Sie erhalten 5 Punkte'),
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text(
+                                      'Folgende Antworten sind falsch:'),
+                                  content: SingleChildScrollView(
+                                    child: ListBody(
+                                      children: [
+                                        for (int i = 0;
+                                            i < answerList.length;
+                                            i++)
+                                          Text(answerList[i]),
+                                        Text(
+                                            '\nBitte beachten Sie dabei folgenden Grund ${widget.questionCard.reason}'),
+                                      ],
+                                    ),
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                        child: const Text('ok'),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        })
                                   ],
-                                ),
-                              ),
-                              actions: [
-                                TextButton(
-                                    child: const Text('ok'),
-                                    onPressed: () {
-                                      Navigator.of(context).pop();
-                                    })
-                              ],
-                            );
-                          });
-                    }
-                  },
-                  label: 'bestätigen'),
-              CustomButton(
-                  onPressed: () {
-                    _mailto(widget.questionCard.id, userId);
-                  },
-                  label: 'Frage melden')
+                                );
+                              });
+                        } else {
+                          currentCounter = await checkCounter();
+                          if (currentCounter == -1) {
+                            currentUserId = userId;
+                            currentCounter = 5;
+                            countPoints += 5;
+                            _addUserRanking();
+                          } else {
+                            int newCounterValue = currentCounter + 5;
+                            countPoints += 5;
+                            await firestoreQuestionRep.updateCounter(
+                                userId, newCounterValue);
+                          }
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text(
+                                      '$userName, Sie haben alles richtig beantwortet'),
+                                  content: const SingleChildScrollView(
+                                    child: ListBody(
+                                      children: [
+                                        Text('Sie erhalten 5 Punkte'),
+                                      ],
+                                    ),
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                        child: const Text('ok'),
+                                        onPressed: () {
+                                          Navigator.of(context).pop();
+                                        })
+                                  ],
+                                );
+                              });
+                        }
+                      },
+                      label: 'bestätigen'),
+                ],
+              )
             ],
-          )
-        ],
-      ),
+          ),
+        ),
+        CustomButton(
+            onPressed: () {
+              _mailto(widget.questionCard.id, userId);
+            },
+            label: 'Frage melden')
+      ],
     );
   }
 
