8b81781ac2ab304964c85ede6714f02805cb3d6d
schwenske <sven.neuefeind@gmx.de>
2024-06-28 12:34:27 +0200

    fertig

--- lib/screens/auth_gate.dart
+++ lib/screens/auth_gate.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import 'home.dart';
 
 class AuthGate extends StatelessWidget {
@@ -12,9 +13,52 @@ class AuthGate extends StatelessWidget {
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
-          return SignInScreen(providers: [
-            EmailAuthProvider(),
-          ]);
+          return SignInScreen(
+            providers: [
+              EmailAuthProvider(),
+            ],
+            headerBuilder: (context, constraints, shrinkOffset) {
+              return SafeArea(
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        const Text(
+                          'Quizzen',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                        const Text(''),
+                        const Text(
+                          'Achtung, verwenden Sie immer ein starkes Passwort!',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        InkWell(
+                          onTap: () async {
+                            if (await canLaunchUrlString(
+                                'https://www.datenschutz.org/passwort-generator/')) {
+                              await launchUrlString(
+                                  'https://www.datenschutz.org/passwort-generator/');
+                            } else {
+                              //
+                            }
+                          },
+                          child: const Text(
+                            'Passwort-Generator von datenschutz.org verwenden',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.blue, // Blau
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
         }
 
         return const HomeScreen();
